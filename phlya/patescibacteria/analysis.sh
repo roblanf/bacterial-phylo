bd630719579e8aa5b48271f006213f62bc8663dd
roblanf <rob.lanfear@gmail.com>
2025-03-04 16:54:38 +1100

    update analysis script
    to work on small alignments
    also start the idea of PMSF on a small subset

--- phlya/patescibacteria/analysis.sh
+++ phlya/patescibacteria/analysis.sh
@@ -1,7 +1,7 @@
 # input files
 phylum="p__Patescibacteria"
 taxonomy="../../raw_data/bac120_taxonomy_r202_clean.tsv"
-alignment="../../raw_data/gtdb_r202_bac120.faa"
+alignment="../../raw_data/bac120_msa_reps_r202.faa"
 tree="../../raw_data/bac120_r202.tree"
 
 # Get the taxa we want - just for the phylum of interest
@@ -17,7 +17,7 @@ faSomeRecords $alignment taxa.txt alignment.faa
 # get the subtree
 nw_labels -I $tree | sort > all_tips_sorted.txt # gets all tip labels
 sort taxa.txt > taxa_sorted.txt
-comm -23 all_tips_sorted.txt tax_sorted.txt > tips_to_prune.txt # gets the tips not in my list
+comm -23 all_tips_sorted.txt taxa_sorted.txt > tips_to_prune.txt # gets the tips not in my list
 nw_prune $tree $(cat tips_to_prune.txt) > subtree.nwk
 nw_topology -I subtree.nwk > subtree_topology.nwk # discard all unnecessary labels and branch lengths
 sed -E 's/^\((.*)\);$/\1;/' subtree_topology.nwk > subtree_topology_clean.nwk
@@ -26,12 +26,29 @@ sed -E 's/^\((.*)\);$/\1;/' subtree_topology.nwk > subtree_topology_clean.nwk
 
 # IQ-TREE analyses
 
-# Basic
-iqtree -s alignment.faa -t subtree_topology_clean.nwk -m Q.pfam+R8 -nt 120 -safe -pre basic
 
-# C20
-iqtree -s alignment.faa -t subtree_topology_clean.nwk -m Q.pfam+C20+R8 -nt 120 -safe -pre C20fixed
+#### Estimate PMSF on the full dataset
+# 1. Basic analysis
+iqtree -s alignment.faa -t subtree_topology_clean.nwk -m Q.pfam+G -nt 60 -safe -pre basic
+
+
+#### Estimate PMSF on a 100 taxon dataset
+
+
+
+#### C20 model no PMSF
+
+iqtree -s alignment.faa -t subtree_topology_clean.nwk -m C20 -nt 60 -safe -pre C20fixed
+
 
 # C20 estimate weights
 iqtree -s alignment.faa -t subtree_topology_clean.nwk -m Q.pfam+C20+R8 -mwopt -nt 120 -safe -pre C20estimated
 
+# CAT-PMSF
+# first optimise the branch length and C20 weights on a fixed starting tree
+iqtree -s alignment.faa -te subtree_topology_clean.nwk -m Q.pfam+C20+R8 -mwopt -nt 60 -safe -pre C20_R8_fixed
+
+# optionally we could infer the GTR matrix here, but that will likely take too long...
+
+# then fix the model and get the PMSF site profiles
+iqtree -s alignment.faa -ft C20_R8_fixed.tree -m Q.pfam+C20{}+R8{} -mwopt -nt 60 -safe -pre CATPMSF_C20
