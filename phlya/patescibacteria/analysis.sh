f419d8be09f85f2a62bbecd06efcd25a152bea5c
roblanf <rob.lanfear@gmail.com>
2025-03-06 21:45:38 +1100

    Update analysis.sh

--- phlya/patescibacteria/analysis.sh
+++ phlya/patescibacteria/analysis.sh
@@ -37,6 +37,8 @@ sed -E 's/^\((.*)\);$/\1;/' subtree_100_topology.nwk > subtree_100_topology_clea
 
 # IQ-TREE analyses
 
+#### C20 on the full dataset
+iqtree -s alignment.faa -t subtree_topology_clean.nwk -m C20 -nt 60 -safe -pre C20fixed
 
 #### Estimate PMSF on the full dataset: bacteria
 # 1. Basic analysis to get the tree
@@ -45,12 +47,8 @@ iqtree -s alignment.faa -t subtree_topology_clean.nwk -m Q.pfam+G -nt 60 -safe -
 iqtree -s alignment.faa -ft basic.treefile -m Q.pfam+C60+G -nt 60 -safe -pre qpfam_c60_g_sf -n 0
 # apply them with +G
 iqtree -s alignment.faa -fs qpfam_c60_g_sf.sitefreq -t basic.treefile -m Q.pfam+C60+G -nt 60 -safe -pre qpfam_c60_g_tree_noboot
-
 # apply them with +G and UFBOOT
 iqtree -s alignment.faa -fs qpfam_c60_g_sf.sitefreq -t basic.treefile -m Q.pfam+C60+G -nt 60 -safe -pre qpfam_c60_g_tree_ufboot -bb 1000
-
-
-
 
 
 #### Estimate PMSF on a 100 taxon dataset: bac3
@@ -61,13 +59,41 @@ iqtree -s alignment_100.faa -t subtree_100_topology_clean.nwk -m Q.pfam+C60+R8 -
 iqtree -s alignment_100.faa -ft 100_tree.treefile -m Q.pfam+C60+G -nt 60 -safe -pre qpfam_c60_g_sf_100 -n 0
 # apply them to the FULL alignment (using the same starting tree as before)
 iqtree -s alignment.faa -fs qpfam_c60_g_sf_100.sitefreq -t basic.treefile -m Q.pfam+C60+G -nt 60 -safe -pre qpfam_c60_g_tree_noboot_100
+# apply them to the FULL alignment with UFBOOT (using the same starting tree as before)
+iqtree -s alignment.faa -fs qpfam_c60_g_sf_100.sitefreq -t basic.treefile -m Q.pfam+C60+G -nt 60 -safe -pre qpfam_c60_g_tree_ufboot_100 -bb 1000
+
+
+#### GTRpmix on 100 taxon dataset, attempt with mwopt: bac2
+# 1. Get the tree with C60 - take it from 100_tree above
+# 2. Estimate GTR+C60 with mwopt
+iqtree -s alignment_100.faa -m GTR20+C60+G4 --link-exchange --init-exchange q.pfam -te 100_tree.treefile -me 0.99 -nt 60 -safe -mwopt -pre GTR_c60_g_mwopt
+# 3. get the site frequencies
+iqtree -s alignment_100.faa -ft 100_tree.treefile -m GTRPMIX+C60+G4 -mdef GTR_c60_g_mwopt.GTRPMIX.nex -nt 60 -safe -pre GTR_c60_g_mwopt_sf_100 -n 0
+# 4. apply them to the FULL alignment (using the same starting tree as before)
+iqtree -s alignment.faa -fs GTR_c60_g_mwopt_sf_100.sitefreq -t basic.treefile -m GTRPMIX+C60+G4 -mdef GTR_c60_g_mwopt.GTRPMIX.nex -nt 60 -safe -pre GTR_c60_g_mwopt_tree_noboot_100
+
+
+
+#### GTRpmix on 100 taxon dataset, site freqs on the whole thing? bacteria
+# 1. Get the tree with C60 - take it from 100_tree above
+# 2. Estimate GTR+C60 with mwopt: as above
+# 3. get the site frequencies
+iqtree -s alignment.faa -ft basic.treefile -m GTRPMIX+C60+G4 -mdef GTR_c60_g_mwopt.GTRPMIX.nex -nt 60 -safe -pre GTR_c60_g_mwopt_sf -n 0
+
+# 4. apply them to the FULL alignment (using the same starting tree as before)
+iqtree -s alignment.faa -fs GTR_c60_g_mwopt_sf.sitefreq -t basic.treefile -m GTRPMIX+C60+G4 -mdef GTR_c60_g_mwopt.GTRPMIX.nex -nt 60 -safe -pre GTR_c60_g_mwopt_tree_noboot_100
 
 
 
 
-#### C20 on the full dataset: bac2
 
-iqtree -s alignment.faa -t subtree_topology_clean.nwk -m C20 -nt 60 -safe -pre C20fixed
+
+
+
+
+
+
+
 
 
 
